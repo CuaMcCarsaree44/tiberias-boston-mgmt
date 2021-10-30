b1d3ee3339569c1d30ca4246e71929edd370cfa3
CuaMcCarsaree44 <yosua_kristianto144@outlook.com>
2021-10-31 04:35:05 +0700

    Updating entities between Member, regency, and district

--- app/Models/Entity/Member.php
+++ app/Models/Entity/Member.php
@@ -8,4 +8,16 @@ class Member extends Model
 {
     protected $table = 'member';
 
+    public function regency() {
+        return $this->belongsTo('App\Models\Entity\Regency', 'regency_id');
+    }
+
+    public function district() {
+        return $this->belongsTo('App\Models\Entity\District', 'district_id');
+    }
+
+    public function documents() {
+        return $this->hasMany('App\Models\Entity\MemberDocument', 'member_id', 'id');
+    }
+
 }
